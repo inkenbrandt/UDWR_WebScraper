c2a0ce7ab54bea33fdd10a5d233ad70b62e7a6ed
Paul <paulinkenbrandt@utah.gov>
2014-07-06 10:42:35 -0600

    Added stuff

---
+++ web_scraper.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 06 10:09:25 2014
+
+@author: Paul
+"""
+
+import scrapy
+
+class waterrights(scrapy.Item):
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
+    
+class DmozSpider(scrapy.Spider):
+    name = "wr"
+    allowed_domains = ["http://www.waterrights.utah.gov/"]
+    start_urls = [
+        "http://waterrights.utah.gov/",
+        "http://waterrights.utah.gov/docSys/v907/"
+    ]
+    
+    
+    '''
+    http://waterrights.utah.gov/docSys/v907/d907/d90704sy.htm
+    http://waterrights.utah.gov/docSys/v907/e907/e907027q.htm
+    http://waterrights.utah.gov/docSys/v907/d907/d90704z5.htm
+    http://waterrights.utah.gov/cgi-bin/docview.exe?Folder=welllog3439
+    http://waterrights.utah.gov/cblapps/wrprint.exe?wrnum=31-661
+     <B>LITHOLOGY:</B>
+   Depth(ft)  Lithologic Description                                                          Color        Rock Type
+   From    To
+
+    '''
